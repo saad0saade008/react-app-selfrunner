c2ef1b4e142a41a4b28edc412e3e7ec138bfe4dc
ahmadqayyum7 <ahmadqayyum19@gmail.com>
2024-04-17 13:51:00 +0500

    changed the output

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello.
+          DevOps Intern at DaFi Labs.
         </p>
         <a
           className="App-link"
