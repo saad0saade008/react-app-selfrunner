e7c21f15a1b5992edb9725eb89b65b4805e6b8bc
saad0saade008 <saad0saade008@gmail.com>
2025-02-19 16:10:53 +0100

    Merged remote changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps.
+          Deployed by Selfhosted Runner.
         </p>
         <a
           className="App-link"
